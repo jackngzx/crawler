35ac0b8c7e8b7028aa493c4e2b6afaa45c46344a
Anh-Vu Nguyen <jackngzx@gmail.com>
2026-06-04 16:07:59 +0800

    adding concurrency

--- main.go
+++ main.go
@@ -6,27 +6,74 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 )
+
+type config struct {
+	pages              map[string]PageData
+	baseURL            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+	maxPages           int
+}
+
+func newConfig(rawBaseURL string, maxConcurrency int, maxPages int) (*config, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse baseURL: %s", err)
+	}
+	return &config{
+		pages:              make(map[string]PageData),
+		baseURL:            baseURL,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, maxConcurrency),
+		wg:                 &sync.WaitGroup{},
+		maxPages:           maxPages,
+	}, nil
+}
 
 func main() {
 	argCount := len(os.Args)
-	if argCount < 2 {
-		fmt.Println("no website provided")
+	if argCount < 4 {
+		fmt.Println("missing arguments: usage ./crawler URL maxConcurrency maxPages")
 		os.Exit(1)
 	}
-	if argCount > 2 {
+	if argCount > 4 {
 		fmt.Println("too many arguments provided")
 		os.Exit(1)
 	}
-	if argCount == 2 {
-		fmt.Println("starting crawl of: ", os.Args[1])
+
+	fmt.Println("starting crawl of: ", os.Args[1])
+
+	maxConcurrency, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	crawledPages := make(map[string]int)
-	crawlPage(os.Args[1], os.Args[1], crawledPages)
+	maxPages, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	fmt.Println(crawledPages)
+	c, err := newConfig(os.Args[1], maxConcurrency, maxPages)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	c.wg.Add(1)
+	go c.crawlPage(os.Args[1])
+	c.wg.Wait()
+
+	for normalizedURL := range c.pages {
+		fmt.Printf("urls crawled: %s\n", normalizedURL)
+	}
+	fmt.Println("finished crawling pages")
 }
 
 func getHTML(rawURL string) (string, error) {
@@ -63,8 +110,18 @@ func getHTML(rawURL string) (string, error) {
 	return string(data), nil
 }
 
-func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
-	parsedBase, err := url.Parse(rawBaseURL)
+func (cfg *config) crawlPage(rawCurrentURL string) {
+	cfg.concurrencyControl <- struct{}{}
+	defer func() {
+		<-cfg.concurrencyControl
+		cfg.wg.Done()
+	}()
+
+	if cfg.pagesLen() >= cfg.maxPages {
+		return
+	}
+
+	parsedBase, err := url.Parse(cfg.baseURL.String())
 	if err != nil {
 		fmt.Println("error parsing rawBaseURL: ", err)
 		return
@@ -77,7 +134,6 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 	}
 
 	if parsedBase.Hostname() != parsedCurrent.Hostname() {
-		fmt.Println("different domain between base and current url")
 		return
 	}
 
@@ -87,30 +143,46 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
 		return
 	}
 
-	val, exists := pages[normCurrent]
-	if exists {
-		fmt.Println("page is already crawled")
-		pages[normCurrent] = val + 1
+	addPage := cfg.addPageVisit(normCurrent)
+	if !addPage {
 		return
-	} else {
-		pages[normCurrent] = 1
 	}
 
-	fmt.Println("getting page html from current url: ", normCurrent)
 	pageHTML, err := getHTML(rawCurrentURL)
 	if err != nil {
 		fmt.Println("error getting html from normCurrent: ", err)
 		return
 	}
-	fmt.Println("page html retrieval successful")
 
-	pageURLs, err := getURLsFromHTML(pageHTML, parsedCurrent)
-	if err != nil {
-		fmt.Println("error getting urls from page: ", err)
-		return
-	}
+	pageData := extractPageData(pageHTML, rawCurrentURL)
+	cfg.setPageData(normCurrent, pageData)
 
-	for _, pageURL := range pageURLs {
-		crawlPage(rawBaseURL, pageURL, pages)
+	for _, pageURL := range pageData.OutgoingLinks {
+		cfg.wg.Add(1)
+		go cfg.crawlPage(pageURL)
 	}
+}
+
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	val, exists := cfg.pages[normalizedURL]
+	if exists {
+		return false
+	} else {
+		cfg.pages[normalizedURL] = val
+		return true
+	}
+}
+
+func (cfg *config) setPageData(normalizedURL string, data PageData) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	cfg.pages[normalizedURL] = data
+}
+
+func (cfg *config) pagesLen() int {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	return len(cfg.pages)
 }
